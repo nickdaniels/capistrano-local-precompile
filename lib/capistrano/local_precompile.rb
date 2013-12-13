826c7f4871c9b879596f6ce86a4496e4e6798b90
Nick Daniels <nick@roadio.com>
2013-12-13 20:54:27 +0000

    Don't delete the manifest file

--- lib/capistrano/local_precompile.rb
+++ lib/capistrano/local_precompile.rb
@@ -39,8 +39,6 @@ module Capistrano
 
             desc "Precompile assets locally and then rsync to app servers"
             task :precompile, :only => { :primary => true }, :on_no_matching_servers => :continue do
-              run "rm -f #{release_path}/#{fetch(:assets_dir)}/manifest-*"
-
               servers = find_servers :roles => assets_role, :except => { :no_release => true }
               servers.each do |srvr|
                 run_locally "#{fetch(:rsync_cmd)} ./#{fetch(:assets_dir)}/ #{user}@#{srvr}:#{release_path}/#{fetch(:assets_dir)}/"
